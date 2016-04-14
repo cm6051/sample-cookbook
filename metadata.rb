5a7e4cdbd9b55a4e21998e062ee2a384dd3bf0b7
Jenkins <jenkins@ip-10-0-6-52.eu-west-1.compute.internal>
2016-04-14 15:33:04 +0000

    Bump version to v0.1.4

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'sendachi@tesco.com'
 license 'All rights reserved'
 description 'Installs/Configures sample-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.3'
+version '0.1.4'
 issues_url 'https://github.com/james-s-nduka/sample-cookbook/issues'
 source_url 'https://github.com/james-s-nduka/sample-cookbook'
