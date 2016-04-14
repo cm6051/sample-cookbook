9764ffb33c5fda8db8604b0126a4477039a01096
Admin <james.s.nduka@synwin.co.uk>
2016-04-14 14:43:34 +0100

    update email address

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'sample-cookbook'
 maintainer 'Tesco'
-maintainer_email 'admin@tesco.com'
+maintainer_email 'sendachi@tesco.com'
 license 'All rights reserved'
 description 'Installs/Configures sample-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
